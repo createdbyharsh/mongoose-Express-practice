610b516e80e67c9d93ba3a0734bd7491beb787ce
createdbyharsh <createdbyharsh@gmail.com>
2025-07-28 13:34:55 +0530

    done

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+var methodOverride = require("method-override");
 
 const app = express();
 const port = 8080;
 
+app.use(methodOverride("_method"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -23,10 +25,6 @@ main()
     console.log(err);
   });
 
-// app.get("/", (req, res) => {
-//   res.send("/ working");
-// });
-
 // Index route
 
 app.get("/", async (req, res) => {
@@ -38,7 +36,7 @@ app.get("/new", (req, res) => {
   res.render("new.ejs");
 });
 
-app.post("/new", async (req, res) => {
+app.post("/", async (req, res) => {
   let { from, to, msg } = req.body;
   let newChat = await Chat.create({
     from: from,
@@ -49,6 +47,26 @@ app.post("/new", async (req, res) => {
   newChat.save().catch((err) => {
     console.log("db error");
   });
+  res.redirect("/");
+});
+
+app.get("/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let data = await Chat.findById(id);
+  console.log(data);
+  res.render("edit.ejs", { data });
+});
+
+app.put("/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let { msg } = req.body;
+  await Chat.findByIdAndUpdate(id, { msg: msg });
+  res.redirect("/");
+});
+
+app.delete("/:id/delete", async (req, res) => {
+  let { id } = req.params;
+  await Chat.findByIdAndDelete(id);
   res.redirect("/");
 });
 
